BitOBSessiOn/android_kernel_nvidia_shieldtablet

media: tegra: nvavp: Fix reloc offset check
- Check whether command buffer data offset is 32-bit
  aligned
- Check whether relocation offset is 32-bit aligned
  and calculated offset is within command buffer size
- Check whether target offset is 32-bit aligned
  and derived address is within target buffer size

Bug 1741516

Change-Id: Ie5370bc1538c8cf9a702904fb88eb850baeb063d
Signed-off-by: Somu Sundaram <somasundaram@nvidia.com>
Reviewed-on: http://git-master/r/1113199
Reviewed-by: Automatic_Commit_Validation_User
GVS: Gerrit_Virtual_Submit
Tested-by: Somu Sundaram <somasundarams@nvidia.com>
Reviewed-by: Jaiprkash Khemkaar <jkhemkar@nvidia.com>
Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

drivers/media/platform/tegra/nvavp/nvavp_dev.c
@@ -1601,7 +1601,8 @@ static int nvavp_pushbuffer_submit_ioctl(struct file *filp, unsigned int cmd,
 		return PTR_ERR(cmdbuf_dmabuf);
 	}
 
-	if (hdr.cmdbuf.offset > cmdbuf_dmabuf->size) {
+	if ((hdr.cmdbuf.offset & 3)
+		|| (hdr.cmdbuf.offset >= cmdbuf_dmabuf->size)) {
 		dev_err(&nvavp->nvhost_dev->dev,
 			"invalid cmdbuf offset %d\n", hdr.cmdbuf.offset);
 		ret = -EINVAL;
@@ -1645,7 +1646,11 @@ static int nvavp_pushbuffer_submit_ioctl(struct file *filp, unsigned int cmd,
 			goto err_reloc_info;
 		}
 
-		if (clientctx->relocs[i].cmdbuf_offset > cmdbuf_dmabuf->size) {
+		if ((clientctx->relocs[i].cmdbuf_offset & 3)
+			|| (clientctx->relocs[i].cmdbuf_offset >=
+				cmdbuf_dmabuf->size)
+			|| (clientctx->relocs[i].cmdbuf_offset >=
+				(cmdbuf_dmabuf->size - hdr.cmdbuf.offset))) {
 			dev_err(&nvavp->nvhost_dev->dev,
 				"invalid reloc offset in cmdbuf %d\n",
 				clientctx->relocs[i].cmdbuf_offset);
@@ -1662,7 +1667,9 @@ static int nvavp_pushbuffer_submit_ioctl(struct file *filp, unsigned int cmd,
 			goto target_dmabuf_fail;
 		}
 
-		if (clientctx->relocs[i].target_offset > target_dmabuf->size) {
+		if ((clientctx->relocs[i].target_offset & 3)
+			|| (clientctx->relocs[i].target_offset >=
+				target_dmabuf->size)) {
 			dev_err(&nvavp->nvhost_dev->dev,
 				"invalid target offset in reloc %d\n",
 				clientctx->relocs[i].target_offset);

sgefant/android_kernel_lenovo_kai

NvBug 1741516
Bug: 27441354
Bug: 29535845
(cherry picked from commit 69291e6a08ddef8fc504d57f5e995fc336ca14c2)
Signed-off-by: Xia Yang <xiay@nvidia.com>
(cherry picked from commit 84f914b695e0b6b8ddb6360f61eb8766af6254fe)

drivers/media/video/tegra/nvavp/nvavp_dev.c
@@ -1296,7 +1296,8 @@ static int nvavp_pushbuffer_submit_ioctl(struct file *filp, unsigned int cmd,
 		goto err_cmdbuf_mmap;
 	}
 
-	if (hdr.cmdbuf.offset > cmdbuf_handle->size) {
+	if ((hdr.cmdbuf.offset & (sizeof(u32) - 1))
+		|| (hdr.cmdbuf.offset >= cmdbuf_handle->size)) {
 		dev_err(&nvavp->nvhost_dev->dev,
 			"invalid cmdbuf offset %d\n", hdr.cmdbuf.offset);
 		ret = -EINVAL;
@@ -1316,7 +1317,11 @@ static int nvavp_pushbuffer_submit_ioctl(struct file *filp, unsigned int cmd,
 			goto err_reloc_info;
 		}
 
-		if (clientctx->relocs[i].cmdbuf_offset > cmdbuf_handle->size) {
+		if ((clientctx->relocs[i].cmdbuf_offset & (sizeof(u32) - 1))
+			|| (clientctx->relocs[i].cmdbuf_offset >=
+				cmdbuf_handle->size)
+			|| (clientctx->relocs[i].cmdbuf_offset >=
+				(cmdbuf_handle->size - hdr.cmdbuf.offset))) {
 			dev_err(&nvavp->nvhost_dev->dev,
 				"invalid reloc offset in cmdbuf %d\n",
 				 clientctx->relocs[i].cmdbuf_offset);
@@ -1339,7 +1344,9 @@ static int nvavp_pushbuffer_submit_ioctl(struct file *filp, unsigned int cmd,
 			return -EPERM;
 		}
 
-		if (clientctx->relocs[i].target_offset > target_handle->size) {
+		if ((clientctx->relocs[i].target_offset & (sizeof(u32) - 1))
+			|| (clientctx->relocs[i].target_offset >=
+				target_handle->size)) {
 			dev_err(&nvavp->nvhost_dev->dev,
 				"invalid target offset in reloc %d\n",
 				clientctx->relocs[i].target_offset);